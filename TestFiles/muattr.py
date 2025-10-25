f3637ee4b4a76675365e0a12e83901f7f8b6509b
BalooRM <richard_ballew@yahoo.com>
2025-10-25 12:47:56 -0400

    added annot output

--- TestFiles/muattr.py
+++ TestFiles/muattr.py
@@ -17,12 +17,11 @@ def printAttr(prefix, obj):
     for attr in attributes:
         print(prefix, f"{attr}: {getattr(obj, attr)}")
 
-
-indir = './'
-
+#indir = './'
+indir = '../StackOverflow/'
 files = os.listdir(indir)
 files = [a for a in files if '.pdf' in a]
-files = ['NumberTest_muformat2.pdf']
+files = ['fillable_form2.pdf', 'fefillable_form2.pdf']
 print(files)
 
 for fname in files:
@@ -33,7 +32,16 @@ for fname in files:
     for page in pdf:
         widgets = page.widgets()
         for widget in widgets:
-            print('dir(widget)', dir(widget))
+            print('widget field_name:', widget.field_name)
+            #print('dir(widget)', dir(widget))
             printAttr('widget', widget)
-            print('dir(widget._annot)', dir(widget._annot))
-            printAttr('widget._annot.flags', widget._annot.flags)
+            #print("'" + widget.field_name + "': '', ") 
+            # print('widget field_type_string:', widget.field_type_string)
+            # print('widget field_value:', widget. field_value)
+            printAttr('widget _annot:', widget._annot)
+            print(widget._annot.info)
+            # print('dir(widget._annot)', dir(widget._annot))
+            # printAttr('widget._annot.flags', widget._annot.flags)
+    pdf.save(fname.split('.')[0] + '_updated.pdf')
+    pdf.close()
+                  
